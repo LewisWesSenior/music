9948fa1398a5a378f257a158629c015f0b660c5a
James Schofield <James Schofield>
2016-12-03 20:15:44 +0000

    Oliver Test

---
+++ resources/views/oliver-landing.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Oliver Alexander</title>
+	<link rel="stylesheet" type="text/css" href="oliver.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="custom.css">
+	<link href="https://fonts.googleapis.com/css?family=Alegreya:400i, Karla:700, Slabo+27px, Montserrat:400,700" rel="stylesheet">
+</head>
+<body>
+
+					<nav class="navbar navbar-default">
+					  <div class="container-fluid">
+					    <!-- Brand and toggle get grouped for better mobile display -->
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					      <a class="navbar-brand" href="#"><img class="bird" src="birdy.png"></a>
+					    </div>
+					      <ul class="nav navbar-nav navbar-right">
+					        <li><a href="#">WORKS</a></li>
+					        <li><a href="#">TOUR DATES</a></li>
+					        <li><a href="#">MULTIMEDIA</a></li>
+					        <li><a href="#">SHEET MUSIC</a></li>
+					        <li><a href="#">ISLAND SONGS</a></li>
+					        <li><a href="#">CONTACT</a></li>
+					        <li><a href="#">SHOP</a></li>
+					      </ul>
+					    </div><!-- /.navbar-collapse -->
+					  </div><!-- /.container-fluid -->
+					</nav>
+
+			<div class="header">
+						<div class="content-position v-helper">
+							<div class="header-title">
+								<h1>OLIVER ALEXANDER</h1>
+							</div>
+						</div>
+			</div>
+			
+			<div class="break">
+			</div>
+
+				<div class="titles titles-border">
+						<h5>ABOUT</h5>
+					<div class="underline"></div>
+				</div>
+
+					<div class="row">
+						<div class="col-md-10 col-md-offset-1">
+							<h2 class="quote">Hi there, I’m glad you found me. My name is Oliver-Alexander and I like to make music.</h2>
+						</div>
+					</div>
+
+			<div class="row mb-190 xs-mb-30">
+				<div class="col-md-8 col-md-offset-2 col-sm-10 col-sml-offset-1">
+					<div class="columns">
+						<p>On this page you can find out what I have been up to lately and maybe even a little bit about where I will go in the future. For me, the greatest thing about being a musician is being in the position to inspire other people.
+
+						I take such pleasure in hearing that people have been motivated to create after hearing my music, whether it be a painting, a poem, their own music or something completely different.
+
+						Music is not a one way street, it is a conversation where the listener's role is as important as the artist's. Well, here you can find my side of it all. What is yours?</p>
+					</div>
+				</div>
+			</div>
+
+				<div class="titles titles-border">
+						<h5>LATEST WORK</h5>
+					<div class="underline"></div>
+				</div>
+
+<script
+  src="http://code.jquery.com/jquery-3.1.1.js"
+  integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+  crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
