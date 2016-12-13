6edd01eab756123346bdcf3624c9175213437089
Lewis Senior <lewis@BlueBoxIT.local>
2016-12-13 19:08:19 +0000

    Clean Up

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,3 @@ Route::get('/', function () {
 route::get('/laravelDHP', function() {
 	return view('welcome');
 });
-
-Route::get('/login', function () {
-    return view('login');
-	
-Route::get('/login1', function () {
-    return view('login');
-	
-//////_--_--_--\\\\\\\\\
-Route::get('/test1', function () {
-    return view('test1');
-	
-	Route::get('/test2', function () {
-    return view('test2');
-	
-	Route::get('/test3', function () {
-    return view('test3');
-	
-	Route::get('/test4', function () {
-    return view('test4');
-	
-});
